72aa239dcdb21423cd81ee5a3947b7726dbefc8a
AS <AS>
2019-11-26 13:21:10 +0200

    .

--- index.js
+++ index.js
@@ -7,48 +7,41 @@ console.log('Starting... (port:'+port+')');
 http.createServer(function (req, res) {
   console.log('Received event:', JSON.stringify(Object.keys(req), null, 2));
 
-  res.writeHead(200, {'Content-Type': 'application/json'});
-  res.end(JSON.stringify({}));
+  switch(req.method) {
+    case 'post':
+      let data = []
+      req.on('data', chunk => {
+        data.push(chunk)
+      })
+      req.on('end', () => {
+        data=JSON.parse(data).todo // 'Buy the milk'
+      })
+      superagent
+        .post(data.url)
+        .send(data.body)
+        .query(data.queryParams)
+        .set(data.headers)
+        .end((err1, res1) => {
+          var proxyResp={};
+          try {
+            proxyResp = {
+              err:err1,
+              res:res1,
+            };
+          } catch(e) {
+            console.log('Try-Catch ERROR: '+e);
+          }
+          res.writeHead(200, {'Content-Type': 'application/json'});
+          res.end(JSON.stringify(proxyResp));
+        });
+      break;
+    default:
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      res.end(JSON.stringify({
+        err: req.method
+      }));
+  }
+
 }).listen(port, ()=>{
   console.log('Server running at port: '+port);
 });
-
-
-// exports.handler = (event, context, callback) => {
-    
-
-//     switch (event.httpMethod) {
-//         // case 'DELETE':
-//         //     dynamo.deleteItem(JSON.parse(event.body), done);
-//         //     break;
-//         case 'GET':
-//             // dynamo.scan({ TableName: event.queryStringParameters.TableName }, done);
-//             break;
-//         case 'POST':
-//           let data = JSON.parse(event.body);
-
-//           superagent
-//             .post(data.url)
-//             .send(data.body)
-//             .query(data.queryParams)
-//             .set(data.headers)
-//             .end((err, res) => {
-//               try {
-//                 let proxyResp = {
-//                   err:err,
-//                   res:res,
-//                 };
-//                 return res.json(proxyResp);
-//               } catch(e) {
-//                 console.log('Try-Catch ERROR: '+e);
-//                 return res.json({err:e});
-//               }
-//             });
-//             break;
-//         // case 'PUT':
-//         //     dynamo.updateItem(JSON.parse(event.body), done);
-//         //     break;
-//         default:
-//             // done(new Error(`Unsupported method "${event.httpMethod}"`));
-//     }
-// };
