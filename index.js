b6c1e418810826d084011479537e1a7ae98129ae
AS <AS>
2019-11-26 12:37:42 +0200

    .

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var port = process.argv[2] || 3000;
+var ip = '35.180.14.57' || "127.0.0.1"
 console.log('port:'+port);
 
 http.createServer(function (req, res) {
@@ -7,5 +8,5 @@ http.createServer(function (req, res) {
 
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n\n');
-}).listen(port, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:'+port+'/');
+}).listen(port, ip);
+console.log('Server running at http://'+ip+':'+port+'/');
